f185a0faee86b40a30a20efae0d469f8c756261a
MattSux <89954184+MattSux@users.noreply.github.com>
2023-02-02 13:39:58 -0600

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,7 +1,7 @@
 <?php
 require('database.php');
 
-
+//Query below:
 $query = 'SELECT customers.customerID, customers.emailAddress, customers.firstName, customers.lastName, addresses.line1, addresses.city, addresses.state, addresses.zipCode, addresses.phone FROM customers INNER JOIN addresses ON customers.customerID = addresses.customerID GROUP BY customers.customerID, customers.emailAddress, customers.firstName, customers.lastName, addresses.line1, addresses.city, addresses.state, addresses.zipCode, addresses.phone';// PUT YOUR SQL QUERY HERE
 // Example: $query = 'SELECT * FROM customers';
 
